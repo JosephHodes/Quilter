eb0b5c7dc92942409e01f7cd6c960446b999fde6
Joseph Hodes <jojoho70@gmail.com>
2021-05-12 18:00:54 -0400

    refractored

--- test.py
+++ test.py
@@ -11,12 +11,19 @@ images = [cv.imread(file) for file in glob.glob(
 def stitch(images=images):
     grey1 = cv.cvtColor(images[0], cv.COLOR_RGB2GRAY)
     grey2 = cv.cvtColor(images[1], cv.COLOR_RGB2GRAY)
+    corners = cornerdetection(grey1)
+    corners2 = cornerdetection(grey2)
+    size = grey1.shape()
+    print(size[1])
+
+
+def cornerdetection(grey1):
     corners = cv.goodFeaturesToTrack(grey1, 25, 0.01, 10)
     corners = np.int0(corners)
     for i in corners:
         x, y = i.ravel()
         cv.circle(grey1, (x, y), 3, 255, -1)
-    plt.imshow(grey1), plt.show()
+        return (x, y)
 
 
 stitch()
