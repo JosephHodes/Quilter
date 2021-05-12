b7e1b3ac77e085d7a5710eb320c7b54d510837e1
Joseph Hodes <jojoho70@gmail.com>
2021-05-12 17:28:06 -0400

    working on stitcher added corner detection

--- test.py
+++ test.py
@@ -1,8 +1,22 @@
 import cv2 as cv
-import tensorflow as tf
 import pandas as pd
-import pillow as pil
-import glob 
+import glob
+import numpy as np
+from matplotlib import pyplot as plt
 
-images = [cv2.imread(file) for file in glob.glob("/Users/joseph/Downloads/18-2/*.png")]
-cv.imshow(images[1])
+images = [cv.imread(file) for file in glob.glob(
+    "/Users/joseph/Downloads/18-2/*.png")]
+
+
+def stitch(images=images):
+    grey1 = cv.cvtColor(images[0], cv.COLOR_RGB2GRAY)
+    grey2 = cv.cvtColor(images[1], cv.COLOR_RGB2GRAY)
+    corners = cv.goodFeaturesToTrack(grey1, 25, 0.01, 10)
+    corners = np.int0(corners)
+    for i in corners:
+        x, y = i.ravel()
+        cv.circle(grey1, (x, y), 3, 255, -1)
+    plt.imshow(grey1), plt.show()
+
+
+stitch()
