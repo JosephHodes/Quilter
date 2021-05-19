ff1e1d14523e8bd69057c7e1654c59a751721f02
Joseph Hodes <jojoho70@gmail.com>
2021-05-19 17:50:02 -0400

    started working on putting togeth ther images

--- test.py
+++ test.py
@@ -9,20 +9,40 @@ images = [cv.imread(file) for file in glob.glob(
 
 
 def stitch(images=images):
+    img1 = images[0]
+    img2 = images[1]
     grey1 = cv.cvtColor(images[0], cv.COLOR_RGB2GRAY)
     grey2 = cv.cvtColor(images[1], cv.COLOR_RGB2GRAY)
-    corners = cornerdetection(grey1)
-    corners2 = cornerdetection(grey2)
+    corners = cornerdetection(grey1, img1)
+    corners2 = cornerdetection(grey2, img2)
+
     lw, lh = grey1.shape
-    offsetx = lw+lw * .90
+    offsetx = int((lw * .90)+lw)
+    offsety = (lh * .90)+lh
+    newimage = np.zeros((lw*2, lh), np.uint8)
+    newimage = cv.merge((newimage, newimage, newimage))
+    newimage = cv.cvtColor(newimage, cv.COLOR_RGB2RGBA)
+    corners[:, :, 3] = 200
+    corners2[:, :, 3] = 200
+
+    newimage[0: lw, 0: lh] = corners[0: lw, 0: lh]
+    newimage[offsetx:lw+lw,
+             0: lh] = (newimage[offsetx:lw+lw, 0: lh] + corners2[:, :])/2
+    cv.imwrite("owo1.png", newimage)
 
 
-def cornerdetection(grey1):
-    corners = cv.goodFeaturesToTrack(grey1, 25, 0.01, 10)
-    corners = np.int0(corners)
-    for i in corners:
-        x, y = i.ravel()
-        cv.circle(grey1, (x, y), 20, 255, -1)
+def cornerdetection(gray, img):
+    gray = np.float32(gray)
+    dst = cv.cornerHarris(gray, 5, 3, .1)
+    print(len(dst))
+    dst = cv.dilate(dst, None)
+
+    img[dst > 0.01*dst.max()] = [0, 0, 255]
+    img = cv.cvtColor(img, cv.COLOR_RGB2RGBA)
+    return img
 
 
 stitch()
+
+if cv.waitKey(0) & 0xff == 27:
+    cv.destroyAllWindows()
